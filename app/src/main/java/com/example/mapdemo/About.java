2b30e6941f46fbc9f3a202bc762dae620708003b
aniket-cr <aniket@iitrpr.ac.in>
2016-02-07 15:00:01 +0530

    About and Instruction Page content added

--- app/src/main/java/com/example/mapdemo/About.java
+++ app/src/main/java/com/example/mapdemo/About.java
@@ -23,6 +23,34 @@ public class About extends ActionBarActivity {
         Typeface custom_font1 = Typeface.createFromAsset(getAssets(),  "fonts/TheMillionMileMan.ttf");
         tx1.setTypeface(custom_font1);
 
+        TextView tx3 = (TextView)findViewById(R.id.textView3);
+        tx3.setText("1) This Game aims at developing a platform to promote exer gaming" +
+                ". Exer Games are video games that are a form of exercise. The prime motive is to promote a healthy and " +
+                "active lifestyle in a fun way.");
+
+        TextView tx2 = (TextView)findViewById(R.id.textView2);
+        tx2.setText("2) We expect the user playing the game to get fitter day by day while playing the game." +
+                " The game is mainly focussed on Obese children of age 10-20 years.");
+
+        TextView tx4 = (TextView)findViewById(R.id.textView4);
+        tx4.setText("3) Game Elements: \n\n i) Game Boundary: A square blue lined boundary is constructed with the " +
+                "player in the center.\n\n" +
+                " ii) Player Bubble: The player is shown as a pink bubble and is at the center of the square as the game starts." +
+                " As the player moves, this Bubble also moves.\n\n" +
+                " iii) Snack Bubble: These bubbles are shown as yellow color and eating this bubble having smaller size than the Player " +
+                "will result in the Player becoming stronger and bigger. However if a bigger snack bubble consumes The Player, " +
+                "the player will end up losing the game.\n\n" +
+                " iv) Junk Bubble:These bubbles are shown as green color and eating a junk Bubble would result in reduced size of the " +
+                "player Bubble. If a larger junk Bubble consumes you, you will end up losing the game.\n\n" +
+                " v) Repeller Bubble: These bubbles are shown as red color and when a player bubble comes near a repeller, the repeller " +
+                "gets repelled from the player bubble and runs away from it.\n\n" +
+                " vi) Blind Spot: These are black colored holes shown on the map and a non player Bubble if goes into such a hole will " +
+                "come out from any of the other blind spots.") ;
+
+        TextView tx5 = (TextView)findViewById(R.id.textView5);
+        //tx5.setVisibility(View.VISIBLE);
+
+        TextView tx6 = (TextView)findViewById(R.id.textView6);
 
 
     }
--- app/src/main/java/com/example/mapdemo/Instructions.java
+++ app/src/main/java/com/example/mapdemo/Instructions.java
@@ -22,6 +22,36 @@ public class Instructions extends ActionBarActivity {
         TextView tx1 = (TextView)findViewById(R.id.about_text);
         Typeface custom_font1 = Typeface.createFromAsset(getAssets(),  "fonts/TheMillionMileMan.ttf");
         tx1.setTypeface(custom_font1);
+
+        TextView tx3 = (TextView)findViewById(R.id.textView3);
+        tx3.setText("1) Click on The Play Button. Chose one of the modes: SinglePlayer or MultiPlayer. Chose the level" +
+                " you want to play: Become the Biggest or The Repulsor Challenge. Chose the Size of the boundary: Large, " +
+                "Medium or Small.");
+
+        TextView tx2 = (TextView)findViewById(R.id.textView2);
+        tx2.setText("2) SinglePlayer Mode: In Become the Biggest, try to consume all the Snack Bubbles (Yellow)" +
+                " that are smaller than you. When you become the biggest out of all the Snack bubbles, you win!" +
+                " If one of the Snack bubbles consumes you or there comes a situation that you cannot become the biggest" +
+                " from that point on, you Lose!");
+
+        TextView tx4 = (TextView)findViewById(R.id.textView4);
+        tx4.setText("3) SinglePlayer Mode: In the Repulsor Challenge, if you consume the Repeller bubble (Red), you win!" +
+                " If one of the Snack Bubbles consumes you, you lose!") ;
+
+        TextView tx5 = (TextView)findViewById(R.id.textView5);
+        tx5.setText("4) MultiPlayer Mode: In Become the Biggest, try to consume all the Snack Bubbles (Yellow)" +
+                " as well as the other player bubbles" +
+                " that are smaller than you. When you become the biggest out of all the Player bubbles, you win!" +
+                " If one of the Snack or Player bubbles consumes you, you lose!") ;
+
+        TextView tx6 = (TextView)findViewById(R.id.textView6);
+        tx6.setText("5) MultiPlayer Mode: If you catch the Repeller Bubble before anybody else, you win! " +
+                "If any of the Snack bubbles or the Player bubbles consumes you, you lose!") ;
+
+        TextView tx7 = (TextView)findViewById(R.id.textView7);
+        tx7.setText("6) If the Player goes outside the boundary due to some reason, he will be given a time " +
+                "window to come back inside. If he fails to do so, he will lose the game.") ;
+
     }
 
 
